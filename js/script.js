80e594187e5f4f82cdf5e4b8aaa6b14d70fdf146
Katherineslove <48259743+Katherineslove@users.noreply.github.com>
2019-07-31 11:42:57 +1200

    updates

--- js/script.js
+++ js/script.js
@@ -1,15 +1,46 @@
-$(document).ready(function() {
+$(document).ready(function(){
+
+
+  $('#ageRangeSlider').slider({
+    range: true,
+    min: 0,
+    max: 100,
+    values: [0,100],
+    slide: function(event, ui){
+
+    },
+    stop: function(event, ui){
+      // console.log(ui.values);
+      $('#currentRange').text(`${ui.values[0]} - ${ui.values[1]}`);
+      minAge = ui.values[0];
+      maxAge = ui.values[1];
+      console.log(minAge);
+      console.log(maxAge);
+    }
+  });
 
   const btn = $('#getPeople');
+
     btn.click(function() {
       // console.log('button had been clicked');
       $.ajax({
         type: 'GET',
-        url: 'people.json',
+        url: 'https://my.api.mockaroo.com/peopledata.json?key=47b0ce70',
         dataType: 'json',
         success: function(data){
           console.log(data);
           console.log('success');
+          data.map(function(person) {
+            let card = '<div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-1">';
+            card += '<div class="card text-center h-100">';
+            card += '<div class="card-body">';
+            card += `<h5 class ="card title">${person['title']} ${person['first_name']} ${person['last_name']}</h5>`;
+            card +=  `<p class="card-text">${person['email']}</p>`;
+            card += '</div>';
+            card += '</div>';
+            card += '</div>';
+            $('#peopleContainer').append(card);
+          })
         },
         error: function(error) {
           console.log(error);
@@ -17,6 +48,10 @@ $(document).ready(function() {
         }
       })
     });
+
+
+
+
 
 
 });
